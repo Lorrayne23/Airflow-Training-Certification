2e2c4944a8f30621343cd3634baa2d55ab668fc1
Lorrayne Reis <loohsilva995@gmail.com>
2023-01-07 12:12:45 -0300

    creating a new group of tasks called checking , inside the task group

--- dags/groups/process_tasks.py
+++ dags/groups/process_tasks.py
@@ -16,9 +16,26 @@ def process_c(partner_name,partner_path):
     print(partner_name)
     print(partner_path)
 
+@task.python
+def check_a():
+    print("checking")
+
+@task.python
+def check_b():
+    print("checking")
+
+
+@task.python
+def check_c():
+    print("checking")
+
 def process_tasks(partner_settings):
     with TaskGroup(group_id='process_tasks') as process_tasks:
         process_a(partner_settings['partner_name'], partner_settings['partner_path'])
         process_b(partner_settings['partner_name'], partner_settings['partner_path'])
         process_c(partner_settings['partner_name'], partner_settings['partner_path'])
+        with TaskGroup(group_id='test_tasks') as test_tasks:
+            check_a()
+            check_b()
+            check_c()
     return process_tasks
