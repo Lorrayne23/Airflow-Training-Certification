69ac2198caffc7c5877486032e81739d8511c0af
Lorrayne Reis <loohsilva995@gmail.com>
2023-01-08 18:16:08 -0300

    using a pool to execute each task at time

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.models import Variable
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.decorators import task , dag
@@ -10,6 +10,7 @@ from datetime import datetime , timedelta
 from airflow.utils.task_group import TaskGroup
 from groups.process_tasks import process_tasks
 from airflow.operators.dummy import DummyOperator
+import time
 
 
 default_args = {
@@ -19,6 +20,17 @@ default_args = {
         default_args=default_args, schedule_interval="@daily",
         dagrun_timeout=timedelta(minutes=10), tags=["data_science"],
         catchup=False, max_active_runs=1)
+
+
+#def _choosing_partner_based_on_day(execution_date):
+        #day = execution_date.day_of_week
+        #if (day == 1):
+            #return 'extract_partner_snowflake'
+        #if (day == 3):
+            #return 'extract_partner_netflix'
+        #if (day == 5):
+            #return 'extract_partner_astronomer'
+        #return stop
 
 
 def my_dag():
@@ -41,16 +53,34 @@ def my_dag():
        "path": "/partners/astronomer"
     }
 
+    
+
+
+
+
 }
-    start = DummyOperator(task_id="start")
+    start = DummyOperator(task_id="start", trigger_rule='dummy' , pool='default_pool')
+
+    """choosing_partner_based_on_day = BrachPythonOperator(
+        task_id = 'choosing_partner_based_on_day',
+        python_callable = _choosing_partner_based_on_day
+    )
+
+    stop = DummyOperator(task_id='stop')"""
+
+    storing = DummyOperator(task_id='storing', trigger_rule='none_failed_or_skipped')
+    
+    #choosing_partner_based_on_day >> stop
+
     for partners, details in partners.items():
 
-        @task.python(task_id=f"extract_{partners}",do_xcom_push=False,multiple_outputs=True)
+        @task.python(task_id=f"extract_{partners}",do_xcom_push=False, pool='partner_pool',multiple_outputs=True)
         def extract(partner_name,partner_path):
+            time.sleep(3)
             return {"partner_name": partner_name,"partner_path": partner_path}
         extracted_values = extract(details['name'], details['path'])
         start >> extracted_values 
-        process_tasks(extracted_values )
+        process_tasks(extracted_values ) >> storing
 
 
    
