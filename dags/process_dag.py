6f73e630b81f709f08a4bf100b45e23df83643e8
Lorrayne Reis <loohsilva995@gmail.com>
2023-01-11 17:22:35 -0300

    dag versioning: add a new task

---
+++ dags/process_dag.py
@@ -0,0 +1,25 @@
+from airflow.models import DAG
+from airflow.decorators import task
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020,1,1)
+}
+
+with  DAG('process_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+
+    @task.python
+    def t1():
+        print("t1")
+    
+
+    @task.python
+    def t2():
+        print("t2")
+
+    @task.python
+    def t3():
+        print("t3")
+
+    t1() >> t2() >> t3()    
